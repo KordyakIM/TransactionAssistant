d4c3c6ff54bb8db0edf37320f82f4b5acf2b2572
KordyakIM <30699602+KordyakIM@users.noreply.github.com>
2021-05-24 10:13:04 +0700

    Yes

--- MessageListener.java
+++
@@ -1,9 +0,0 @@
-package ru.project.MobileAssistant;
-
-public interface MessageListener {
-    /**
-     * To call this method when new message received and send back
-     * @param message Message
-     */
-    void messageReceived(String message);
-}
